6b3a89382e0e23ef5d3be55054494985c53b892c
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-05 03:11:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(name)
-  
+def parrot(phrase)
+  puts "Pretty Bird!"
   return "Squawk!"
 end
+
+parrot()
